8791e57f1657e09f371bc0a07bb9ba64a110448a
saikrishna <saikrishna.4369@gmail.com>
2023-12-10 22:37:39 +0100

    basic configurations

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+import { v4 as uuidv4 } from 'uuid';
+console.log(uuidv4()); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+ 
